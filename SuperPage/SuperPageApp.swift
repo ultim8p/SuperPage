44b0e26c13848076e234eeba43ba6fab126636ac
Guerson <ultim8p@gmail.com>
2023-03-26 08:12:22 +0200

    Initial Commit

---
+++ SuperPage/SuperPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperPageApp.swift
+//  SuperPage
+//
+//  Created by Guerson Perez on 3/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SuperPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
