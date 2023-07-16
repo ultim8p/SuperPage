cbbdd83c4b229169cefb8f301c4450b778f06f48
Guerson <ultim8p@gmail.com>
2023-07-16 05:14:47 +0400

    Update model versions.

--- SuperPage/Features/Architecture/Chat/Model/AIModel/AIModel.swift
+++ SuperPage/Features/Architecture/Chat/Model/AIModel/AIModel.swift
@@ -9,21 +9,35 @@ import Foundation
 
 enum AIModel: String, Codable {
     
+    // GPT 3
+    
     case gpt35turbo = "gpt-3.5-turbo"
     
     case gpt35turbo0301 = "gpt-3.5-turbo-0301"
     
-    case claudeInstantV1 = "claude-instant-v1"
-        
-    case claudeV1dot3 = "claude-v1.3"
+    case gpt35turbo0613 = "gpt-3.5-turbo-0613"
+    
+    // GPT 4
     
     case gpt4 = "gpt-4"
     
     case gpt40314 = "gpt-4-0314"
     
+    case gpt40613 = "gpt-4-0613"
+    
     case gpt432k = "gpt-4-32k"
     
     case gpt432k0314 = "gpt-4-32k-0314"
+    
+    // Claude Instant
+    
+    case claudeInstantV1 = "claude-instant-v1"
+        
+    // Claude 1.3
+    
+    case claudeV1dot3 = "claude-v1.3"
+    
+    case calude1dot3 = "claude-1.3"
     
     var botName: String {
         return rawValue
